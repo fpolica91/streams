1fd20a352041cf4be6b59076c5c4881e3b3ff8aa
Fabricio <fabriciopolicarpo0@gmail.com>
2021-01-21 03:07:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const {PassThrough} = require('stream')
+const fs = require('fs')
+
+const passThrought = new PassThrough()
+
+process.stdin.pipe(passThrought).pipe(process.stdout)
+// .pipe(process.stdout);
+
+
+// this is used to write input from command to file.txt
+const ouputStream = fs.createWriteStream('file.txt')
+process.stdin.pipe(ouputStream)
