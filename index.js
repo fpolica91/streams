ee5a229253832da4b06199d980b217b3f635d508
Fabricio <fabriciopolicarpo0@gmail.com>
2021-01-21 03:16:42 +0000

    proces stdout

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ process.stdin.pipe(passThrought).pipe(process.stdout)
 // this is used to write input from command to file.txt
 const ouputStream = fs.createWriteStream('file.txt')
 process.stdin.pipe(ouputStream)
+
+const inputStream = fs.createReadStream('example.txt')
+
+inputStream.pipe(process.stdout)
